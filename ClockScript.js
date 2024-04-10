f6ccb3815f63b14ff486f90ba4039ec1066fa6c9
ReinBass <goulartcaio131@gmail.com>
2024-04-10 19:32:37 -0300

    full commit

---
+++ ClockScript.js
@@ -0,0 +1,13 @@
+function Clock() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    const currentTime = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").innerText = currentTime;
+}
+
+setInterval(Clock, 1000);
+
+Clock();
